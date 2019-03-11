497b71d7ea40fa7e47e9e60e3be799e1567ebf25
Robin Mooney <robinmooney.student@careerdevs.com>
2019-03-11 15:10:49 +0000

    added a index.js file to listen for requests, also installed express package

---
+++ index.js
@@ -0,0 +1,9 @@
+const express = require('express');
+
+//setup express app
+const app = express();
+
+//listens for requests
+app.listen(process.env.port || 4000,function(){
+    console.log("now listening for requests");
+});
